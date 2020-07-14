4f996902b25a48aa8673ce81b4037817ed4a8173
zorgonide <ammar1998@gmail.com>
2020-07-15 02:19:35 +0530

    Monngoose Population

--- models/dishes.js
+++ models/dishes.js
@@ -14,9 +14,9 @@ var commentSchema = new Schema({
         type: String,
         required: true
     },
-    author:  {
-        type: String,
-        required: true
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }
 }, {
     timestamps: true
